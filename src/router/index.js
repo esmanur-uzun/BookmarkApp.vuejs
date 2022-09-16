7539091eb5c7fcca557da19dd98d2b52c56af06e
esmanurbtmz <esmabitmez@hotmail.com>
2022-09-16 12:11:58 +0300

    project started

---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+
+const routes = [
+    {
+        name: "HomePage",
+        path : "/"
+    }
+]
+
+export default createRouter({
+    routes : [],
+    history : createWebHashHistory()
+})
